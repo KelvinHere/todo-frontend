56c0a669373de8d4920471f942cc11e82c7f6e5f
kelvinhere <kwardeu@gmail.com>
2022-12-29 16:59:58 +0000

    FEAT: Setup react-bootstrap

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from "react-bootstrap/Button"
+import 'bootstrap/dist/css/bootstrap.min.css' // Bring in Bootstrap CSS
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hi</p>
+      <Button>Hihi</Button>
+    </div>
+  );
+}
+
+export default App;
