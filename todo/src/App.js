3f4611db236a534ae2a402673c6a07a504f667e7
kelvinhere <kwardeu@gmail.com>
2022-12-29 17:12:50 +0000

    FEAT: Add bootstrap container grid with title and body

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import Button from "react-bootstrap/Button"
 import 'bootstrap/dist/css/bootstrap.min.css' // Bring in Bootstrap CSS
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button"
+import Toast from "react-bootstrap/Toast"
+
 
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
-      <Button>Hihi</Button>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>To-Do</h1>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button>To-Do</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
